5b63a08180a154cb97ed25a248e5530e9e78486b
Wilfred Obruche O <wilfredcloudspace@gmail.com>
2023-02-24 02:29:45 +0000

    Update notifications_activiies reply

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -15,7 +15,7 @@ class NotificationsActivities:
         'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
         'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Worf',
-        'message': 'This post has no honor!',
+        'message': 'This post has honor!',
         'likes_count': 0,
         'replies_count': 0,
         'reposts_count': 0,
