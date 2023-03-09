deae9fcb94a21919d99aa71c2ca0ad53beb0ae6a
Wilfred Obruche O <wilfredcloudspace@gmail.com>
2023-03-09 03:53:35 +0000

    Fix Logger not defined

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -4,8 +4,8 @@ from opentelemetry import trace
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  def run():
-    LOGGER.info('Hello Cloudwatch! from  /api/activities/home')
+  def run(Logger):
+    Logger.info('Hello Cloudwatch! from  /api/activities/home')
     with tracer.start_as_current_span("home-activities-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
