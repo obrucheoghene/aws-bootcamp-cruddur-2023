e055af82b8f558ba53979076899484cb6ca87df2
Wilfred Obruche O <wilfredcloudspace@gmail.com>
2023-03-12 12:43:34 +0000

    Update checkAuth method

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -35,16 +35,44 @@ export default function HomeFeedPage() {
     }
   };
 
-  const checkAuth = async () => {
-    console.log('checkAuth')
-    // [TODO] Authenication
-    if (Cookies.get('user.logged_in')) {
+  // const checkAuth = async () => {
+  //   console.log('checkAuth')
+  //   // [TODO] Authenication
+  //   if (Cookies.get('user.logged_in')) {
+  //     setUser({
+  //       display_name: Cookies.get('user.name'),
+  //       handle: Cookies.get('user.username')
+  //     })
+  //   }
+  // };
+
+
+  // check if we are authenicated
+const checkAuth = async () => {
+  Auth.currentAuthenticatedUser({
+    // Optional, By default is false. 
+    // If set to true, this call will send a 
+    // request to Cognito to get the latest user data
+    bypassCache: false 
+  })
+  .then((user) => {
+    console.log('user',user);
+    return Auth.currentAuthenticatedUser()
+  }).then((cognito_user) => {
       setUser({
-        display_name: Cookies.get('user.name'),
-        handle: Cookies.get('user.username')
+        display_name: cognito_user.attributes.name,
+        handle: cognito_user.attributes.preferred_username
       })
-    }
-  };
+  })
+  .catch((err) => console.log(err));
+};
+
+// check when the page loads if we are authenicated
+React.useEffect(()=>{
+  loadData();
+  checkAuth();
+}, [])
+
 
   React.useEffect(()=>{
     //prevents double call
