3828fa8e3b889d0bd3e787666ca5e8a2a45f2f18
Wilfred Obruche O <111436934+wilfredcloud@users.noreply.github.com>
2023-03-24 08:18:22 +0000

    Refactor db activities

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,13 +1,12 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
-from lib.db import pool, query_wrap_array
-
+from lib.db import db
 # tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run(cognito_user_id=None):
     #logger.info("HomeActivities")
-    sql = query_wrap_array("""
+   results = db.query_array_json("""
       SELECT
         activities.uuid,
         users.display_name,
@@ -23,16 +22,5 @@ class HomeActivities:
       LEFT JOIN public.users ON users.uuid = activities.user_uuid
       ORDER BY activities.created_at DESC
     """)
-    print("SQL--------------")
-    print(sql)
-    print("SQL--------------")
-    with pool.connection() as conn:
-      with conn.cursor() as cur:
-        cur.execute(sql)
-        # this will return a tuple
-        # the first field being the data
-        json = cur.fetchone()
-    print("-------------------------- JSON")
-    print(json[0])
-    return json[0]
+
     return results
